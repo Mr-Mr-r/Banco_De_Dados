b2c04fb9b49a64f9522c328f65c32d75c7d7b760
Mr-Mr-r <101223477+Mr-Mr-r@users.noreply.github.com>
2022-04-11 20:24:38 -0300

    Update AGRI.sql

--- AGRI.sql
+++ AGRI.sql
@@ -5,4 +5,26 @@ id integer PRIMARY KEY,
 nome VARCHAR(50),
 idade YEAR(4),
 contato varchar(30),
+telefone int(12),
+RG int(7),
+CPF int(11),
+)
+
+CREATE TABLE Endereco(
+id integer PRIMARY KEY,
+CEP int(8),
+endereco VARCHAR(50),
+numero int(5),
+bairro VARCHAR(30),
+cidade VARCHAR(30),
+complemento VARCHAR(15),
+)
+
+CREATE TABLE Produtos(
+id integer PRIMARY KEY,
+nome VARCHAR(30),
+quantidade int(5),
+tipo VARCHAR(20),
+modelo YEAR(4),
+fabricantes VARCHAR(30)
 )
